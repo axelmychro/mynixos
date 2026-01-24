277c5747dd5eee84404bdcb271224f6fabaa92e1
Axel <axelmychro@gmail.com>
2026-01-24 17:31:52 +0700

    delete hardware file, split host from main config

--- src/nixos/configuration.nix
+++ src/nixos/configuration.nix
@@ -1,140 +1,62 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./hosts/prts.nix
+  ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-  networking.hostName = "prts-nix"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  time.timeZone = "Asia/Jakarta";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  services.dbus.enable = true;
-  # Enable the X11 windowing system.
-  services.xserver = {
+  hardware.graphics = {
     enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+    enable32Bit = true;
   };
-  services.xserver.displayManager.gdm.wayland = true;
 
-  
-  
-  services.gnome.gnome-keyring.enable = true;
+  services = {
+    dbus.enable = true;
+    power-profiles-daemon.enable = true;
+    xserver.enable = true;
+    desktopManager.plasma6.enable = true;
 
-  
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  };
+  xdg.portal.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+    jack.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.axel = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-  };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
 
   fonts.packages = with pkgs; [
+    nerd-fonts.ubuntu-sans
     nerd-fonts.fira-code
   ];
 
-  environment.systemPackages = with pkgs; [
-    swaylock swayidle waybar wofi
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam" "steam-unwrapped"
+  ];
 
-    foot nano vim
+  programs = {
+    steam = {
+      enable = true;
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    alacritty nano vim
     wget curl rsync openssh git
     ripgrep fd tree bat fzf which eza
     btop htop duf ncdu fastfetch oh-my-posh
     zip unzip p7zip xz
-
-    wl-clipboard mako
-
-    librewolf vscodium
   ];
 
-  programs.foot.enable = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 }
-
---
+++ src/nixos/hosts/prts.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+{
+  networking.hostName = "prts-nix";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Asia/Jakarta";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.users.axel = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  services.xserver.videoDrivers = [ "intel" ];
+
+  environment = {
+    sessionVariables = {
+      ELECTRON_DISABLE_GPU=1;
+    };
+    systemPackages = with pkgs; [
+      librewolf vscodium
+    ];
+  };
+}

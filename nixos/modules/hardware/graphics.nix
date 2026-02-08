df3e4f054ae4a9e3e5b605c5c60c7da9dce1ba5f
Axel <axelmychro@gmail.com>
2026-02-08 20:46:10 +0700

    remove broken experiments

--- nixos/modules/hardware/graphics.nix
+++ nixos/modules/hardware/graphics.nix
@@ -15,14 +15,6 @@
       open = true;
       nvidiaSettings = true;
       powerManagement.enable = true;
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
     };
   };
   environment.systemPackages = with pkgs; [

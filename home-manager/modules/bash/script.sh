7c6f016fb63bdc2e79688cf78e7f77a31cf02967
Axel <axelmychro@gmail.com>
2026-02-09 19:42:33 +0700

    cut, standardize, and add aliases

--- home-manager/modules/bash/script.sh
+++ home-manager/modules/bash/script.sh
@@ -1,3 +1,50 @@
+my() {
+  case "$1" in
+    ""|help|-h|--help)
+      cat <<'EOF'
+usage: my <command>
+
+commands:
+  drun
+  test
+  switch
+  undo
+  update
+  look
+EOF
+      ;;
+    switch)
+      git add /etc/nixos \
+        && sudo nixos-rebuild switch --flake /etc/nixos#mychro \
+        && reboot
+      ;;
+    drun)
+      sudo nixos-rebuild dry-run --flake /etc/nixos#mychro
+      ;;
+    test)
+      git add /etc/nixos \
+        && sudo nixos-rebuild test --flake /etc/nixos#mychro
+      ;;
+    undo)
+      sudo nixos-rebuild switch --rollback && reboot
+      ;;
+    update)
+      git add /etc/nixos \
+        && nix flake update --extra-experimental-features nix-command \
+        && sudo nixos-rebuild switch --flake /etc/nixos#mychro \
+        && reboot
+      ;;
+    look)
+      fd -t f /etc/nixos /etc/nixos -E flake.lock -E result \
+        -x sh -c 'echo "--- {} ---"; bat --style=plain --paging=never "{}"'
+      ;;
+    *)
+      echo "my: $1: command not found"
+      return 1
+      ;;
+  esac
+}
+
 rm() {
   if [[ "$*" == *"-rf /"* ]]; then
     echo "idiot"

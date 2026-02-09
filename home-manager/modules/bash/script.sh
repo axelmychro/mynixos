b9e0669a37002a909d9c670b2fd2b31ae1228bb3
Axel <axelmychro@gmail.com>
2026-02-09 19:43:14 +0700

    conditional clear and fastfetch

--- home-manager/modules/bash/script.sh
+++ home-manager/modules/bash/script.sh
@@ -46,17 +46,18 @@ EOF
 }
 
 rm() {
-  if [[ "$*" == *"-rf /"* ]]; then
-    echo "idiot"
-  else
-    command rm "$@"
-  fi
+  for arg in "$@"; do
+    [[ "$arg" == "/" ]] && { echo "idiot."; return 1; }
+  done
+  command rm "$@"
 }
 
 mcd() {
-  mkdir -p "$1" && cd "$1"
+  [[ -z "$1" ]] && return 1
+  mkdir -p -- "$1" && cd -- "$1"
 }
 
 eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/catppuccin_frappe.omp.jsonc)"
-clear -x
-fastfetch
+
+[[ $- == *i* ]] && clear
+[[ $SHLVL -eq 1 ]] && fastfetch

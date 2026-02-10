33b1fca8b1093328c5695bbf7435436eb0a0a652
Axel <axelmychro@gmail.com>
2026-02-10 08:50:32 +0700

    ref: split plasma config and move wallpaper to assets folder

---
+++ home-manager/modules/plasma/interface/desktop.nix
@@ -0,0 +1,46 @@
+{ ... }:
+let
+  sans = "UbuntuSans Nerd Font";
+  code = "FiraCode Nerd Font";
+in
+{
+  programs.plasma = {
+    workspace = {
+      wallpaper = ../assets/desktop.jpg;
+      iconTheme = "breeze-dark";
+      cursor = {
+        animationTime = 5;
+        cursorFeedback = "Bouncing";
+        size = 36;
+        taskManagerFeedback = true;
+        theme = "Breeze_Light";
+      };
+    };
+    fonts = {
+      general = {
+        family = sans;
+        pointSize = 12;
+      };
+      fixedWidth = {
+        family = code;
+        pointSize = 12;
+      };
+      small = {
+        family = sans;
+        pointSize = 10;
+      };
+      toolbar = {
+        family = sans;
+        pointSize = 12;
+      };
+      menu = {
+        family = sans;
+        pointSize = 12;
+      };
+      windowTitle = {
+        family = sans;
+        pointSize = 12;
+      };
+    };
+  };
+}

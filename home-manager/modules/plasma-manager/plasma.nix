93e0d7b588fbacd3d3884866ba6825d2d346011c
Axel <axelmychro@gmail.com>
2026-02-10 13:05:42 +0700

    configure plasma behaviour

--- home-manager/modules/plasma-manager/plasma.nix
+++ home-manager/modules/plasma-manager/plasma.nix
@@ -19,6 +19,15 @@
         general.askForConfirmationOnLogout = false;
         sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
       };
+      kscreenlocker = {
+        lockOnResume = true; # resume = after sleep
+        timeout = 5; # minutes before screen is locked
+      };
+      krunner = {
+        activateWhenTypingOnDesktop = true;
+        historyBehavior = "disabled";
+        position = "top";
+      };
     };
   };
   imports = [

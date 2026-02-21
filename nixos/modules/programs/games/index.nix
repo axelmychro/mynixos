8e254ae6e156bfc64feb32e7dd9727a5e6f87967
Axel <axelmychro@gmail.com>
2026-02-21 16:25:49 +0700

    fix: add missing steam path

--- nixos/modules/programs/games/index.nix
+++ nixos/modules/programs/games/index.nix
@@ -1,1 +1,13 @@
-_: { imports = [ ./steam/index.nix ]; }
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [ ./steam/index.nix ];
+  services.flatpak.packages = [
+    "com.vysp3r.ProtonPlus"
+  ];
+  environment.systemPackages = with pkgs; [
+    osu-lazer-bin
+  ];
+}
--- nixos/modules/programs/index.nix
+++ nixos/modules/programs/index.nix
@@ -1,9 +1,9 @@
 _: {
   imports = [
-    ./games.nix
+    ./games/index.nix
     ./utilities/index.nix
     ./cli.nix
-    ./development.nix
+    ./development.nix   
     ./documents.nix
     ./internet.nix
     ./multimedia.nix

495cb2e666c99f7e7816ed58717ae288989f897d
Axel <axelmychro@gmail.com>
2026-02-25 20:30:41 +0700

    ref: removed pinned apps for system status

--- home-manager/modules/plasma-manager/interface/panels.nix
+++ home-manager/modules/plasma-manager/interface/panels.nix
@@ -1,8 +1,8 @@
 _:
-let
-  browser = "app.zen_browser.zen.desktop";
-  editor = "dev.zed.Zed.desktop";
-in
+# let
+#   browser = "app.zen_browser.zen.desktop";
+#   editor = "dev.zed.Zed.desktop";
+# in
 {
   programs.plasma.panels = [
     {
@@ -18,21 +18,36 @@ in
             icon = "nix-snowflake";
           };
         }
-        "org.kde.plasma.pager"
         {
-          iconTasks = {
-            launchers = [
-              "applications:${browser}"
-              "applications:${editor}"
-            ];
+          pager = {
+            general = {
+              showWindowOutlines = false;
+              showApplicationIconsOnWindowOutlines = false;
+              showOnlyCurrentScreen = false;
+              navigationWrapsAround = false;
+              displayedText = "desktopName";
+            };
           };
         }
+        # {
+        #   iconTasks = {
+        #     launchers = [
+        #       "applications:${browser}"
+        #       "applications:${editor}"
+        #     ];
+        #   };
+        # }
+        "org.kde.plasma.systemmonitor.cpu"
+        "org.kde.plasma.systemmonitor.memory"
+        "org.kde.plasma.systemmonitor.diskactivity"
+        "org.kde.plasma.panelspacer"
         {
           digitalClock = {
             date.format.custom = "ddd, d MMM";
             calendar.firstDayOfWeek = "monday";
           };
         }
+        "org.kde.plasma.panelspacer"
         {
           systemTray = {
             items = {

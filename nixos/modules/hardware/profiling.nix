a7f6819923649252a6ea1b1e935cdb698a95f36c
Axel <axelmychro@gmail.com>
2026-02-28 09:12:39 +0700

    ref: switch to auto-cpufreq profiling

--- nixos/modules/hardware/profiling.nix
+++ nixos/modules/hardware/profiling.nix
@@ -1,9 +1,15 @@
-_: {
+{ pkgs, ... }:
+{
+
+  environment.systemPackages = with pkgs; [
+    thermald
+    auto-cpufreq
+  ];
   services = {
     thermald.enable = true;
 
     # major options below all conflict with each other
-    power-profiles-daemon.enable = true;
+    power-profiles-daemon.enable = false;
 
     tlp = {
       enable = false;
@@ -14,20 +20,26 @@ _: {
     };
 
     auto-cpufreq = {
-      enable = false;
+      enable = true;
       settings = {
         battery = {
           governor = "powersave";
           turbo = "never";
           energy_performance_preference = "power";
+          scaling_max_freq = 1600000;
+
+          ideapad_laptop_conservation_mode = true;
+          enable_thresholds = true;
+          start_threshold = 60;
+          stop_threshold = 80;
         };
         charger = {
           governor = "powersave";
           turbo = "never";
           energy_performance_preference = "power";
+          scaling_max_freq = 1600000;
         };
       };
     };
   };
-
 }

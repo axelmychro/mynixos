acc79e971a6b5e89a0d4125f3fd20ed30b134b30
Axel <axelmychro@gmail.com>
2026-03-01 10:24:10 +0700

    feat: enable 32bit alsa

--- system/modules/hardware/audio.nix
+++ system/modules/hardware/audio.nix
@@ -2,8 +2,12 @@ _: {
   services.pipewire = {
     enable = true;
     audio.enable = true;
-    alsa.enable = true;
-    jack.enable = true;
+    wireplumber.enable = true;
     pulse.enable = true;
+    jack.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
   };
 }

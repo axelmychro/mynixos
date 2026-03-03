e7e323e4dfb5aebb5152956835bc1528cc849f95
Axel <axelmychro@gmail.com>
2026-03-03 17:57:16 +0700

    feat: system monitor on panel

--- user/axel/home-manager/plasma-manager/interface/panels.nix
+++ user/axel/home-manager/plasma-manager/interface/panels.nix
@@ -7,17 +7,55 @@ _: {
       height = 36;
       hiding = "none";
       lengthMode = "fill";
+
       widgets = [
         "org.kde.plasma.showdesktop"
         {
-          pager = {
-            general = {
-              showWindowOutlines = false;
-              showApplicationIconsOnWindowOutlines = false;
-              showOnlyCurrentScreen = false;
-              navigationWrapsAround = false;
-              displayedText = "desktopName";
-            };
+          pager.general = {
+            showWindowOutlines = false;
+            showApplicationIconsOnWindowOutlines = false;
+            showOnlyCurrentScreen = false;
+            navigationWrapsAround = false;
+            displayedText = "desktopName";
+          };
+        }
+        {
+          systemMonitor = {
+            title = "Memory";
+            displayStyle = "org.kde.ksysguard.textonly";
+            sensors = [
+              {
+                name = "memory/physical/usedPercent";
+                label = "";
+                color = "129,200,190";
+              }
+            ];
+          };
+        }
+        {
+          systemMonitor = {
+            title = "CPU";
+            displayStyle = "org.kde.ksysguard.textonly";
+            sensors = [
+              {
+                name = "cpu/all/usage";
+                label = "";
+                color = "140,170,238";
+              }
+            ];
+          };
+        }
+        {
+          systemMonitor = {
+            title = "GPU";
+            displayStyle = "org.kde.ksysguard.textonly";
+            sensors = [
+              {
+                name = "gpu/gpu0/usage";
+                label = "󰈈";
+                color = "166,209,137";
+              }
+            ];
           };
         }
         "org.kde.plasma.panelspacer"
@@ -29,26 +67,50 @@ _: {
         }
         "org.kde.plasma.panelspacer"
         {
-          systemTray = {
-            items = {
-              shown = [
-                "org.kde.plasma.networkmanagement"
-                "org.kde.plasma.battery"
-                "org.kde.plasma.volume"
-              ];
-              hidden = [
-                "org.kde.plasma.clipboard"
-                "org.kde.plasma.bluetooth"
-                "org.kde.plasma.mediacontrol"
-                "org.kde.plasma.brightness"
-              ];
-            };
+          systemMonitor = {
+            title = "Temperature";
+            displayStyle = "org.kde.ksysguard.textonly";
+            sensors = [
+              {
+                name = "cpu/all/maximumTemperature";
+                label = "";
+                color = "231,130,132";
+              }
+            ];
           };
         }
         {
-          kickoff = {
-            icon = "nix-snowflake";
+          systemMonitor = {
+            title = "Storage";
+            displayStyle = "org.kde.ksysguard.textonly";
+            sensors = [
+              {
+                name = "disk/all/usedPercent";
+                label = "";
+                color = "241,240,186";
+              }
+            ];
           };
+        }
+        {
+          systemTray.items = {
+            shown = [
+              "org.kde.plasma.networkmanagement"
+              "org.kde.plasma.volume"
+              "org.kde.plasma.battery"
+              "org.kde.plasma.brightness"
+            ];
+
+            hidden = [
+              "org.kde.plasma.clipboard"
+              "org.kde.plasma.bluetooth"
+              "org.kde.plasma.mediacontrol"
+            ];
+          };
+        }
+
+        {
+          kickoff.icon = "nix-snowflake";
         }
       ];
     }

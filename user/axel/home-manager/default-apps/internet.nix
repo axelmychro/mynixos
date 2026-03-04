7c61b266d92095bd1f4b14c216aaaa2d0e9f2206
Axel <axelmychro@gmail.com>
2026-03-04 21:27:56 +0700

    fix: mimeapps name

--- user/axel/home-manager/default-apps/internet.nix
+++ user/axel/home-manager/default-apps/internet.nix
@@ -1,6 +1,6 @@
 _: {
   xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/http" = [ "app.zen_browser.zen.desktop" ];
-    "x-scheme-handler/https" = [ "app.zen_browser.zen.desktop" ];
+    "x-scheme-handler/http" = [ "zen.desktop" ];
+    "x-scheme-handler/https" = [ "zen.desktop" ];
   };
 }

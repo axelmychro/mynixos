44f5c50d797052752361d24ced3a305a95144c91
Axel <axelmychro@gmail.com>
2026-03-11 09:46:52 +0700

    ref: enable railway

--- user/axel/modules/programs/games/aagl.nix
+++ user/axel/modules/programs/games/aagl.nix
@@ -2,9 +2,9 @@
 {
   nix.settings = aagl.nixConfig;
   programs = {
-    anime-games-launcher.enable = false;
+    anime-games-launcher.enable = true;
     anime-game-launcher.enable = true;
-    honkers-railway-launcher.enable = false;
+    honkers-railway-launcher.enable = true;
     honkers-launcher.enable = false;
     wavey-launcher.enable = false;
     sleepy-launcher.enable = false;

ff440be17b5b1d99613d29993e62ef9780f67c0c
Axel <axelmychro@gmail.com>
2026-03-15 12:42:30 +0700

    feat: more git abbrs

--- ecosystem/skadi/user/axel/home-manager/shell/fish/abbrs/git.nix
+++ ecosystem/skadi/user/axel/home-manager/shell/fish/abbrs/git.nix
@@ -1,10 +1,44 @@
 _: {
   programs.fish.shellAbbrs = {
     g = "git";
+
+    gs = "git status";
+
+    gd = "git diff";
+    gds = "git diff --staged";
+
+    gl = "git log";
+    glog = "git log --oneline --graph --decorate --all";
+
     ga = "git add";
     gaa = "git add .";
+
     gc = "git commit -m";
+    gca = "git commit --amend";
+
     gco = "git checkout";
-    gp = "git push";
+    gcob = "git checkout -b";
+
+    gb = "git branch";
+    gbd = "git branch -d";
+
+    gsh = "git stash";
+    gsha = "git stash apply";
+    gshp = "git stash pop";
+    gshl = "git stash list";
+
+    gr = "git rebase";
+    gri = "git rebase -i";
+
+    gf = "git fetch";
+
+    gpl = "git pull";
+    gplr = "git pull --rebase";
+
+    gph = "git push";
+    gphf = "git push --force";
+    gphl = "git push --force-with-lease";
+
+    grh = "git reset --hard";
   };
 }

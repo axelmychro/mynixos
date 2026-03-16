3d3897abd60dbc8f33acfe3d1f66a44a29837ed6
Axel <axelmychro@gmail.com>
2026-03-16 18:29:38 +0700

    ref: instant boot

--- ecosystem/skadi/system/configuration.nix
+++ ecosystem/skadi/system/configuration.nix
@@ -1,10 +1,15 @@
-{ pkgs, noctalia, ... }:
 {
-  services.displayManager.ly.enable = true;
+  pkgs,
+  noctalia,
+  ...
+}:
+{
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
   environment.systemPackages = with pkgs; [
     xwayland-satellite
+    cage
+    regreet
     noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     wl-clipboard
     nautilus
---
+++ ecosystem/skadi/system/modules/startup.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  skadiAssets,
+  ...
+}:
+{
+  boot.loader.timeout = 0;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.cage}/bin/cage -sd -- ${pkgs.greetd.regreet}/bin/regreet";
+        user = "greeter";
+      };
+    };
+  };
+  users.users.greeter.extraGroups = [
+    "video"
+    "render"
+  ];
+  programs.regreet = {
+    enable = true;
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+    font = {
+      package = pkgs.nerd-fonts.zed-mono;
+      name = "ZedMono Nerd Font";
+      size = 16;
+    };
+  };
+}

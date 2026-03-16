2ca5eedeb9662992f35f9f67094efd39ab71d49a
Axel <axelmychro@gmail.com>
2026-03-16 13:27:52 +0700

    ref: add wl-clipboard, again

--- ecosystem/skadi/system/configuration.nix
+++ ecosystem/skadi/system/configuration.nix
@@ -1,14 +1,14 @@
 { pkgs, noctalia, ... }:
 {
+  services.displayManager.ly.enable = true;
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.swaylock = { };
   environment.systemPackages = with pkgs; [
     xwayland-satellite
     noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    wl-clipboard
     nautilus
     nnn
   ];
-  services.displayManager.ly.enable = true;
   imports = [ ./modules/index.nix ];
 }

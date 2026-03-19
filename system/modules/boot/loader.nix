6b81628012a4068a81999d8ee7406fb7c92caf93
Axel <axelmychro@gmail.com>
2026-03-19 10:40:27 +0700

    init: grub font

--- system/modules/boot/loader.nix
+++ system/modules/boot/loader.nix
@@ -1,18 +1,34 @@
-_: {
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    systemd-boot.enable = false;
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-      gfxmodeBios = "1920x1080";
-      gfxpayloadBios = "keep";
-      gfxmodeEfi = "1920x1080";
-      gfxpayloadEfi = "keep";
+{ pkgs, ... }:
+{
+  boot = {
+    loader = {
+      timeout = 10;
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = false;
+        configurationLimit = 10;
+        consoleMode = "0";
+      };
+      grub = {
+        enable = true;
+        configurationName = "neeks";
+        configurationLimit = 10;
+        efiSupport = true;
+        useOSProber = true;
+        device = "nodev";
+        gfxmodeEfi = "1920x1080";
+        gfxpayloadEfi = "keep";
+        gfxmodeBios = "1920x1080";
+        gfxpayloadBios = "keep";
 
-      theme = ./assets/catppuccin-frappe-grub-theme;
-      splashImage = ./assets/splashImage.png;
+        font = ./assets/FiraCodeNerdFont-Regular.ttf;
+        fontSize = 16;
+
+        backgroundColor = "#ff000";
+        # theme = ./assets/catppuccin-frappe-grub-theme;
+        # splashImage = ./assets/splashImage.png;
+        splashMode = "normal";
+      };
     };
   };
 }

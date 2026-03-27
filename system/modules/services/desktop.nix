46814cbe6bbea480cb9d0c557ca1ab19811d8603
Axel <axelmychro@gmail.com>
2026-03-27 11:33:45 +0700

    ref: reconfigure font rendering

--- system/modules/services/desktop.nix
+++ system/modules/services/desktop.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }:
 {
   services.xserver.excludePackages = [ pkgs.xterm ];
+  fonts = {
+    fontconfig = {
+      enable = true;
+      subpixel.rgba = "rgb";
+      hinting = {
+        enable = true;
+        style = "full";
+      };
+    };
+    packages = with pkgs; [
+      nerd-fonts.geist-mono
+    ];
+  };
 }
